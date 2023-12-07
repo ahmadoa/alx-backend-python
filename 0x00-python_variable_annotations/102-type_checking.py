b73d3f534c6e28bc5c423fd583d36bdac0749757
ahmadoa <ouladaouida@gmail.com>
2023-12-07 20:38:26 +0000

    validate py code with mypy

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""102-type_checking"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """zoom_array function
+
+    Args:
+        lst (Tuple): list on integers
+        factor (int, optional): integer number defaults to 2
+
+    Returns:
+        List: list of zoomed in
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
