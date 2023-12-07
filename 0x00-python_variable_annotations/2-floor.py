4fab71362e1adcf9b77107c29832e3244cffdab1
ahmadoa <ouladaouida@gmail.com>
2023-12-07 19:36:58 +0000

    tyupe-annotated number flooring function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""2-floor"""
+
+
+def floor(n: float) -> int:
+    """floor function
+
+    Args:
+        n (float): number to floor
+
+    Returns:
+        int: floored number
+    """
+    return int(n)
