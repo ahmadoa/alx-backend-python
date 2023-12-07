e2c7270283ccc54dcf5d5d452257d0935745717a
ahmadoa <ouladaouida@gmail.com>
2023-12-07 19:42:31 +0000

    function that convert float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""3-to_str"""
+
+
+def to_str(n: float) -> str:
+    """converts float to string
+
+    Args:
+        n (float): number to convert
+
+    Returns:
+        str: converted number
+    """
+    return str(n)
