83091763b109bb946f1936b85771d87fb161a8db
ahmadoa <ouladaouida@gmail.com>
2023-12-07 19:57:39 +0000

    type-annotated function to_kv takes string and int or float and returns tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""7-to_kv"""
+from typing import Union, Tuple
+
+
+def to_kv(K: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ returns a tuple of a string and a float
+
+    Args:
+        k (str): string
+        v (Union[int, float]): float or integer
+
+    Returns:
+        Tuple[str, float]: tuple of a string and a float
+    """
+    return (k, float(v**2))
