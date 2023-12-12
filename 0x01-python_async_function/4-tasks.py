4d32e54946095ef7a355b4f0742989c2659ab8cc
ahmadoa <ouladaouida@gmail.com>
2023-12-12 00:06:49 +0000

    typo fixed

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -14,5 +14,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: list of delays
     """
-    tasks = [task_wait_random(max_delay) from _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
